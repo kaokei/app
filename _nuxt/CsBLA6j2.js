371cfa1dc52fe051a638e0083c2bef140d13c7d9
kaokei <kaokei.rst@gmail.com>
2024-12-26 21:13:57 +0800

    Deploying to gh-pages from @ kaokei/tool@2fd2c4eae7104839c017bdd9050419164b760a81 🚀

--- _nuxt/CsBLA6j2.js
+++
@@ -1,1 +0,0 @@
-import{q as s}from"./DyZM1QfF.js";import{u}from"./913o3H0G.js";function i(e,a){const{title:r,titleTemplate:o,...m}=e;return u({title:r,titleTemplate:o,_flatMeta:m},{...a,transform(t){const n=s({...t._flatMeta});return delete t._flatMeta,{...t,meta:n}}})}export{i as u};

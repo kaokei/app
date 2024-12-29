62cab4580b8bb5472b59ba1713f6d4470a417ac0
kaokei <kaokei.rst@gmail.com>
2024-12-30 01:09:18 +0800

    Deploying to gh-pages from @ kaokei/tool@bbdd8f1d300422477ff5da970763a543147648d4 🚀

---
+++ _nuxt/BqYTeqG3.js
@@ -0,0 +1,1 @@
+import{q as s}from"./CHk2Xhgo.js";import{u}from"./CIXwWB26.js";function i(e,a){const{title:r,titleTemplate:o,...m}=e;return u({title:r,titleTemplate:o,_flatMeta:m},{...a,transform(t){const n=s({...t._flatMeta});return delete t._flatMeta,{...t,meta:n}}})}export{i as u};
